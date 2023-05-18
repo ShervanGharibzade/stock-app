784d299e12a506869e7404e72468d2597f696e76
shervan <shervangh@gmail.com>
2023-05-18 03:50:52 +0330

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {},
-    distDir: 'build',
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {}
+
+module.exports = nextConfig
