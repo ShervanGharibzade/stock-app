344cbaa506b6fb395b2d1ccc3c807fa91c8d9c7c
shervan <hosein.g@pirelly.com>
2023-10-26 16:41:27 +0330

    change logic and responsive it

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,40 @@
 "use client";
 
-import React,{useState,useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import Welcome from "@/components/welcome";
 import SearchStock from "@/components/searchStock";
 import LineChart from "@/components/lineChart";
-import Loading from "@/components/loading";
+import { baseUrl, keyApi } from "@/config";
 
 export default function Home() {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [name, setName] = useState('IBM');
-
-  useEffect(()=>{
-    const fetchStock = async() => {
-      const KEY_API = 'H5BSP36WTSGCDNPE';
-      const BASE_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=${KEY_API}`;
-      const res = await fetch(BASE_URL);
-      const data = await res.json();
-      setData(data);
-      setLoading(false)
-    }
-    fetchStock()
-  },[])
-
-
-  if(loading){
-    return <Loading/>
-  }
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [info, setInfo] = useState({
+    symbol: "AAPL",
+    timespan: "day",
+  });
 
   return (
-    <main className="flex h-[100vh] gap-10 justify-center items-center w-fit mx-auto">
-      <div className="h-full grid justify-center items-center">
-      <Welcome/>
-      <SearchStock getSearchResult={(result, stock) =>{
-      setData(result);
-      setName(stock);
-      }}/>
+    <main className="grid py-16 lg:flex h-[100vh] gap-10 justify-center items-center w-fit mx-auto">
+      <div className="h-full grid gap-5 lg:gap-0 justify-center items-center w-1/2">
+        <Welcome />
+        <div className="block lg:hidden">
+          {data && (
+            <LineChart name={info.symbol} loading={loading} data={data} />
+          )}
+        </div>
+        <SearchStock
+          loading={loading}
+          setLoading={setLoading}
+          info={info}
+          setInfo={setInfo}
+          data={data}
+          setData={setData}
+        />
       </div>
-      <LineChart loading={loading} name={name} data={data}/>
+      <div className="hidden lg:block w-1/2">
+        {data && <LineChart name={info.symbol} loading={loading} data={data} />}
+      </div>
     </main>
-  )
+  );
 }
