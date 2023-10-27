17e5574eddde2cee305f06377f91e42d709e193b
shervan <hosein.g@pirelly.com>
2023-10-27 13:34:02 +0330

    change button

--- src/components/searchStock.js
+++ src/components/searchStock.js
@@ -1,6 +1,3 @@
-"use client";
-
-import Image from "next/image";
 import { keyApi, baseUrl } from "@/config";
 import axios from "axios";
 
@@ -11,8 +8,9 @@ export default function SearchStock({
   setData,
   setLoading,
 }) {
-  const loadingButton = "bg-blue-400/60 rounded-full p-5 animate-pulse";
-  const button = "bg-zinc-700 z-50 rounded-full text-white p-3";
+  const loadingButton = "bg-blue-400/60 rounded-full p-5 animate-pulse ";
+  const button =
+    "bg-zinc-700 z-50 rounded-full text-white w-16 h-16 text-zinc-300";
 
   const handleSubmit = async (e, data) => {
     e.preventDefault();
@@ -34,22 +32,16 @@ export default function SearchStock({
       className="w-fit flex justify-center items-center gap-5 mx-auto">
       <input
         type="text"
-        className="rounded-2xl mx-auto p-2 w-42 h-10 outline-none bg-zinc-700 text-white text-center placeholder:text-center z-50"
+        className="rounded-3xl mx-auto p-2 w-42 h-16 outline-none bg-zinc-700 text-white text-center placeholder:text-center z-50"
         placeholder="Symbol"
         onChange={(e) => {
           const uppercasedSymbol = e.target.value.toUpperCase();
           setInfo({ ...info, symbol: uppercasedSymbol });
         }}
+        value={info.symbol}
       />
       <button className={loading ? loadingButton : button} type="submit">
-        {!loading && (
-          <Image
-            src="/images/icons8-submit-progress-50.png"
-            alt="submit progress"
-            width={30}
-            height={30}
-          />
-        )}
+        {!loading && "Sub"}
       </button>
     </form>
   );
